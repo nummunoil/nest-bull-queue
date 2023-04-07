e68aae5b4613f43d772729578b5f96623def5819
Oil <sudarat@1moby.com>
2023-04-07 13:58:04 +0700

    add delay

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -7,10 +7,10 @@ export class QueueService {
   constructor(@InjectQueue('audio') private audioQueue: Queue) {}
 
   async addQueue(): Promise<string> {
-    await this.audioQueue.add('transcode', { foo: 'bar1' });
-    await this.audioQueue.add('transcode', { foo: 'bar2' });
-    await this.audioQueue.add('transcode', { foo: 'bar3' });
-    await this.audioQueue.add('transcode', { foo: 'bar4' });
+    await this.audioQueue.add('transcode', { foo: 'bar1' }, { delay: 5000 });
+    await this.audioQueue.add('transcode', { foo: 'bar2' }, { delay: 5000 });
+    await this.audioQueue.add('transcode', { foo: 'bar3' }, { delay: 5000 });
+    await this.audioQueue.add('transcode', { foo: 'bar4' }, { delay: 5000 });
     console.log('add queue ');
 
     return 'Hello World!';
